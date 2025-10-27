4445feccbee95512b8da9c62a0fadafc9bdd210b
Alluri Thirumaleswari <allurithirumaleswari@gmail.com>
2025-10-27 16:40:32 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,44 @@
+import streamlit as st
+st.header("ALLURI THIRUMALESWARI")
+
+st.subheader("Data Science Intern")
+st.divider()
+st.markdown("#### Career Objective")
+st.markdown('''Motivated and detail-oriented graduate eager to begin a professional career in the IT industry. Skilled in Python, Java, SQL, and 
+web technologies, with a strong foundation in problem-solving and analytical thinking. Open to learning new tools, platforms, and 
+methodologies to adapt quickly to project needs. Committed to continuous growth while contributing positively to organizational 
+success.''')
+st.divider()
+st.markdown("#### Technical skills")
+st.markdown("Programming Languages: Python, Java ")
+st.write("Web Technologies:      HTML, CSS")
+st.write("Databases:             SQL")
+st.write("Tools& platforms:      VS code, Git-Hub")
+st.write("Common skills:         Excel, Word, MS office")
+st.divider()
+st.markdown("#### Projects")
+st.write('''My project “Detection of Mental State” aims to analyze EEG (Electroencephalogram) signal data 
+         to determine a person’s mental condition, such as stress, relaxation, or focus.  The system uses
+          the Support Vector Machine (SVM) algorithm to classify brainwave patterns with high accuracy. 
+         EEG data is preprocessed and key features are extracted to improve the reliability of predictions.
+          A Django web interface is developed to make the results interactive and user-friendly. This project 
+         contributes to understanding mental health patterns and supports earlydetection using machine learning techniques.''')
+st.write('''My project “Restaurant Website” is a user-friendly web application designed to explore menus, order 
+         food online, and book tables easily.It provides a visually appealing interface with sections for menu, 
+         about, and contact details.Users can browse various dishes, view prices, and place orders conveniently.
+         The website ensures a smooth and responsive experience on all devices.This project helps restaurants manage
+         customer interactions efficiently through a digital platform.''')
+st.divider()
+st.markdown("#### Certificates")
+st.write("Software Engineering and Agile Software Development – Infosys Springboard ")
+st.write("Big Data Computing – NPTEL (IIT Madras)" )
+st.write("Python Programming Certificate – Infosys Springboard")
+
+
+
+
+
+
+
+
+
